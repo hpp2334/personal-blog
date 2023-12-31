ce8fd29160285359520b33a30ad9120beec91cab
hpp2334 <hpp2334@outlook.com>
2023-12-31 23:11:16 +0800

    fix: __VUE_PROD_DEVTOOLS__  error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
+const webpack = require('webpack');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        __VUE_OPTIONS_API__: false,
+        __VUE_PROD_DEVTOOLS__: false,
+      })
+    );
+    return config;
+  },
 };
 
 module.exports = nextConfig
